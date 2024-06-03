7e21bf0cef6feb9d94ea573356700dca2925d00b
Magdy <ahassan@turndigital.net>
2024-06-03 16:02:14 +0300

    handle background color

--- lib/miniplayer.dart
+++ lib/miniplayer.dart
@@ -193,20 +193,17 @@ class _MiniplayerState extends State<Miniplayer> with TickerProviderStateMixin {
                           ),
                         );
                       },
-                      child: Material(
-                        child: Container(
-                          constraints: BoxConstraints.expand(),
-                          child: widget.builder(height, _percentage),
-                          // decoration: BoxDecoration(
-                          //   boxShadow: <BoxShadow>[
-                          //     BoxShadow(
-                          //         color: Colors.black45,
-                          //         blurRadius: widget.elevation,
-                          //         offset: Offset(0.0, 4))
-                          //   ],
-                          //   color: Theme.of(context).canvasColor,
-                          // ),
-                        ),
+                      child: SizedBox.expand(
+                        child: widget.builder(height, _percentage),
+                        // decoration: BoxDecoration(
+                        //   boxShadow: <BoxShadow>[
+                        //     BoxShadow(
+                        //         color: Colors.black45,
+                        //         blurRadius: widget.elevation,
+                        //         offset: Offset(0.0, 4))
+                        //   ],
+                        //   color: Theme.of(context).canvasColor,
+                        // ),
                       ),
                     ),
                     // onTap: () => _snapToPosition(_dragHeight != widget.maxHeight
